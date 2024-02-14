e90549d14db0e6fd0b6e26dbb3349b67fb7fd9ba
osamasabry9 <osamasabry324@gmail.com>
2024-02-14 21:13:14 +0200

    added stripe cubit and states

---
+++ lib/features/checkout/presentation/cubit/stripe_states.dart
@@ -0,0 +1,15 @@
+part of 'stripe_cubit.dart';
+
+
+abstract class StripeStates {} 
+
+class StripeInitial extends StripeStates {}
+
+class StripeLoading extends StripeStates {}
+
+class StripeSuccess extends StripeStates {}
+
+class StripeError extends StripeStates {
+  final String message;
+  StripeError({required this.message});
+}
