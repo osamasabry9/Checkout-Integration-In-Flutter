1755367fb071022a25c8643f155b8514222269de
osamasabry9 <osamasabry324@gmail.com>
2024-02-15 13:56:37 +0200

    added models files - added customer input model - added ephemeral key model - added Payment Intent Input Model - added Init Payment Sheet Input Model

---
+++ lib/features/checkout/data/models/init_payment_sheet_input_model.dart
@@ -0,0 +1,11 @@
+class InitPaymentSheetInputModel {
+  final String clientSecret;
+  final String customerId;
+  final String ephemeralKey;
+
+  InitPaymentSheetInputModel({
+    required this.clientSecret,
+    required this.customerId,
+    required this.ephemeralKey,
+  });
+}
